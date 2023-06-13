0d90ab5d355c053410884cd3e15cdeb1832ef696
JSDev1999 <rameshlearningadvance@gmail.com>
2023-06-13 11:51:14 +0530

    updated

--- script.js
+++ script.js
@@ -2,9 +2,31 @@ async function fetchData() {
   const res = await fetch("https://api.coronavirus.data.gov.uk/v1/data");
   const record = await res.json();
   console.log(record);
-  document.getElementById("date").innerHTML = record.data[0].date;
-  document.getElementById("areaName").innerHTML = record.data[0].areaName;
-  document.getElementById("latestBy").innerHTML = record.data[0].latestBy;
-  document.getElementById("deathNew").innerHTML = record.data[0].deathNew;
+  if (record && record?.length && record?.data?.length) {
+    const tableTr = document.getElementById("table_row");
+
+    record.data.forEach((item) => {
+      const tr = document.createElement("tr");
+
+      const dateTd = document.createElement("td");
+      dateTd.textContent = item.date;
+      tr.appendChild(dateTd);
+
+      const areaNameTd = document.createElement("td");
+      areaNameTd.textContent = item.areaName;
+      tr.appendChild(areaNameTd);
+
+      const latestByTd = document.createElement("td");
+      latestByTd.textContent = item.latestBy;
+      tr.appendChild(latestByTd);
+
+      const deathNewTd = document.createElement("td");
+      deathNewTd.textContent = item.deathNew;
+      tr.appendChild(deathNewTd);
+
+      tableTr.appendChild(tr);
+    });
+  }
 }
+
 fetchData();
