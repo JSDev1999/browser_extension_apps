6dc27b9e7dbe8d15a381f20ce45e09a2824d06e7
JSDev1999 <rameshlearningadvance@gmail.com>
2023-06-13 12:08:26 +0530

    updated

--- script.js
+++ script.js
@@ -1,32 +1,20 @@
 async function fetchData() {
-  const res = await fetch("https://api.coronavirus.data.gov.uk/v1/data");
+  const res = await fetch("https://icanhazdadjoke.com/", {
+    headers: {
+      Accept: "application/json",
+    },
+  });
   const record = await res.json();
   console.log(record);
-  if (record && record?.length && record?.data?.length) {
-    const tableTr = document.getElementById("table_row");
-
-    record.data.forEach((item) => {
-      const tr = document.createElement("tr");
-
-      const dateTd = document.createElement("td");
-      dateTd.textContent = item.date;
-      tr.appendChild(dateTd);
-
-      const areaNameTd = document.createElement("td");
-      areaNameTd.textContent = item.areaName;
-      tr.appendChild(areaNameTd);
-
-      const latestByTd = document.createElement("td");
-      latestByTd.textContent = item.latestBy;
-      tr.appendChild(latestByTd);
-
-      const deathNewTd = document.createElement("td");
-      deathNewTd.textContent = item.deathNew;
-      tr.appendChild(deathNewTd);
-
-      tableTr.appendChild(tr);
-    });
+  if (record?.status == 200) {
+    const joke = document.getElementById("joke");
+    joke.innerHTML = record.joke;
   }
 }
 
+function getNewJoke() {
+  fetchData();
+}
+
+document.getElementById("fetchButton").addEventListener("click", getNewJoke);
 fetchData();
